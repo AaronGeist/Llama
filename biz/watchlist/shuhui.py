0ff8a7cbcf5f0f1d85bd928192fb94df02c45b02
maojun.zy <maojun.zy@alipay.com>
2019-07-07 10:04:40 +0800

    Add ShuHui comic checker.

---
+++ biz/watchlist/shuhui.py
@@ -0,0 +1,45 @@
+import json
+
+from core.db import Cache
+from core.emailSender import EmailSender
+from util.utils import HttpUtils
+
+
+class ShuHuiWatchDog:
+    BUCKET_NAME_PREFIX = "SHU_HUI_COMIC_"
+
+    INVALID_CHAPTER_NUM = -1
+
+    @staticmethod
+    def check_and_notify(animation_id):
+        bucket_name = ShuHuiWatchDog.BUCKET_NAME_PREFIX + str(animation_id)
+        previous_chapter_num = Cache().get(bucket_name)
+        if previous_chapter_num is None:
+            previous_chapter_num = -1
+        current_chapter_num = ShuHuiWatchDog.get_max_chapter_num(animation_id)
+
+        if current_chapter_num == ShuHuiWatchDog.INVALID_CHAPTER_NUM:
+            EmailSender.send("", "")
+        elif current_chapter_num > previous_chapter_num:
+            EmailSender.send("", "")
+            Cache().set(bucket_name, current_chapter_num)
+
+    @staticmethod
+    def get_max_chapter_num(animation_id):
+        # not sure what this ver means, could be any value and still works
+        verification = "3b230956"
+
+        response = HttpUtils.get(
+            "https://prod-api.ishuhui.com/ver/{0}/anime/detail?id={1}&type=comics&.json".format(verification,
+                                                                                                animation_id),
+            return_raw=True)
+        if response.status_code != 200 and response.status_code != 301:
+            return ShuHuiWatchDog.INVALID_CHAPTER_NUM
+        else:
+            comic_data = json.loads(response.text)
+            max_chapter_num = int(comic_data["data"]["comicsIndexes"]["1"]["maxNum"])
+            return max_chapter_num
+
+
+if __name__ == "__main__":
+    ShuHuiWatchDog.check_and_notify(1)
