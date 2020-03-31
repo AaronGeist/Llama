7d145753b7c5323b44d9e34227639a73e79559b0
maojun.zy <maojun.zy@antfin.com>
2020-03-31 11:57:20 +0800

    Replace hash with hashlib to ensure hash value's stability.

--- core/tts.py
+++ core/tts.py
@@ -1,7 +1,7 @@
 import os
 import string
 import subprocess
-import sys
+import hashlib
 from urllib.parse import quote
 
 from util.config import Config
@@ -9,25 +9,25 @@ from util.utils import HttpUtils
 
 
 class TextToSpeech:
+    md5 = hashlib.md5()
+
     @classmethod
     def convert(cls, text, audio_file_path):
-        url = 'http://tts.baidu.com/text2audio?idx=1&tex=%s&cuid=baidu_speech_demo&cod=2&lan=zh&ctp=1&pdt=1&spd=5&per=0&vol=5&pit=5' % text
+        url = 'http://tts.baidu.com/text2audio?idx=1&tex=%s&cuid=baidu_speech_demo&cod=1&lan=zh&ctp=1&pdt=1&spd=4&per=5&vol=5&pit=7' % text
         url = quote(url, safe=string.printable)
-        HttpUtils.download_file(url, audio_file_path)
+        HttpUtils.download_file(url, audio_file_path, over_write=True)
 
     @classmethod
     def convert_and_play(cls, text):
-        audio_file_path = os.path.join(Config.get("tts_path"), "tts-%s.mp3" % hash(text))
+        cls.md5.update(text.encode("utf-8"))
+        audio_file_path = os.path.join(Config.get("tts_path"), "tts-%s.mp3" % cls.md5.hexdigest())
+        print(audio_file_path)
 
-        if not os.path.isfile(audio_file_path):
-            # if not exist, download
-            cls.convert(text, audio_file_path)
+        cls.convert(text, audio_file_path)
 
         # play
         subprocess.call(["mpv", audio_file_path])
 
 
 if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        exit()
-    TextToSpeech.convert_and_play(sys.argv[1])
+    TextToSpeech.convert_and_play("小朋友，你好，今天去哪里玩了")
